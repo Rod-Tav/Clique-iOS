7b13ad9d642b5cafd740cc3ef6f3d6fa619a8283
Rod Tavangar <96751400+Rod-Tav@users.noreply.github.com>
2025-11-25 16:46:10 -0500

    VERSION 1.7.4

--- Clique/Services/Helpers/API Setup/AppConfig.swift
+++ Clique/Services/Helpers/API Setup/AppConfig.swift
@@ -24,5 +24,5 @@ struct AppConfig {
     static let currentEnvironment: ServerEnvironment = .development
     static let serverURL: URL = currentEnvironment.serverURL
     
-    static let currentVersion: String = "1.7.3"
+    static let currentVersion: String = "1.7.4"
 }
